bf38d02724a0498165a3203859212f5a5f81d233
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-10-20 05:20:57 +0200

    outline tls initialization

--- src/tls/mod.rs
+++ src/tls/mod.rs
@@ -76,54 +76,71 @@ where
         let mut bucket_ptr = bucket.load(Ordering::Acquire);
 
         if bucket_ptr.is_null() {
-            let new_bucket = allocate_bucket(thread.bucket_size());
-
-            match bucket.compare_exchange(
-                ptr::null_mut(),
-                new_bucket,
-                Ordering::AcqRel,
-                Ordering::Acquire,
-            ) {
-                Ok(_) => bucket_ptr = new_bucket,
-                // If the bucket value changed (from null), that means
-                // another thread stored a new bucket before we could,
-                // and we can free our bucket and use that one instead.
-                Err(other) => unsafe {
-                    let _ = Box::from_raw(ptr::slice_from_raw_parts_mut(
-                        new_bucket,
-                        thread.bucket_size(),
-                    ));
-
-                    bucket_ptr = other;
-                },
-            }
+            self.initialize(bucket, &mut bucket_ptr, thread);
         }
 
         unsafe {
             let entry = &*bucket_ptr.add(thread.index);
 
             // Relaxed: Only the current thread can set the value.
-            if entry.present.load(Ordering::Relaxed) {
-                (*entry.value.get()).assume_init_ref()
-            } else {
-                // Insert the new element into the bucket.
-                entry.value.get().write(MaybeUninit::new(create()));
-
-                // Release: Necessary for iterators.
-                entry.present.store(true, Ordering::Release);
-
-                self.threads.fetch_add(1, Ordering::Relaxed);
-
-                // SeqCst: Synchronize with the fence in `retire`:
-                // - If this fence comes first, the thread retiring will see the new thread
-                //   count and our entry.
-                // - If their fence comes first, we will see the new values of any pointers
-                //   being retired by that thread.
-                atomic::fence(Ordering::SeqCst);
-
-                (*entry.value.get()).assume_init_ref()
+            if !entry.present.load(Ordering::Relaxed) {
+                self.populate(entry, create);
             }
+
+            (*entry.value.get()).assume_init_ref()
         }
+    }
+
+    #[cold]
+    #[inline(never)]
+    fn initialize(
+        &self,
+        bucket: &AtomicPtr<Entry<T>>,
+        bucket_ptr: &mut *mut Entry<T>,
+        thread: Thread,
+    ) {
+        let new_bucket = allocate_bucket(thread.bucket_size());
+
+        match bucket.compare_exchange(
+            ptr::null_mut(),
+            new_bucket,
+            Ordering::AcqRel,
+            Ordering::Acquire,
+        ) {
+            Ok(_) => *bucket_ptr = new_bucket,
+            // If the bucket value changed (from null), that means
+            // another thread stored a new bucket before we could,
+            // and we can free our bucket and use that one instead.
+            Err(other) => unsafe {
+                let _ = Box::from_raw(ptr::slice_from_raw_parts_mut(
+                    new_bucket,
+                    thread.bucket_size(),
+                ));
+
+                *bucket_ptr = other;
+            },
+        }
+    }
+
+    #[cold]
+    #[inline(never)]
+    fn populate(&self, entry: &Entry<T>, create: impl Fn() -> T) {
+        // Insert the new element into the bucket.
+        unsafe {
+            entry.value.get().write(MaybeUninit::new(create()));
+        }
+
+        // Release: Necessary for iterators.
+        entry.present.store(true, Ordering::Release);
+
+        self.threads.fetch_add(1, Ordering::Relaxed);
+
+        // SeqCst: Synchronize with the fence in `retire`:
+        // - If this fence comes first, the thread retiring will see the new thread
+        //   count and our entry.
+        // - If their fence comes first, we will see the new values of any pointers
+        //   being retired by that thread.
+        atomic::fence(Ordering::SeqCst);
     }
 
     /// Load the
