0f41a677bccb60bdd8f3fb66a9d3e0744a5a99e8
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-10-20 21:00:16 +0200

    expose utils::CachePadded

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,4 @@ pub mod reclaim;
 pub use collector::{AsLink, Collector, Link, Linked};
 pub use deferred::Deferred;
 pub use guard::{unprotected, Guard, LocalGuard, OwnedGuard, UnprotectedGuard};
+pub use utils::CachePadded;
